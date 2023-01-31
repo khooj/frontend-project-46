19cd40ba85d5964ede9466fe9f28041f1a470af2
Igor Gilmutdinov <bladoff@gmail.com>
2023-01-31 16:40:08 +0300

    Json format.

--- src/formatters/index.js
+++ src/formatters/index.js
@@ -1,10 +1,12 @@
 import stylish from './stylish.js';
 import plain from './plain.js';
+import json from './json.js';
 
 export default (format) => {
   switch (format) {
     case 'stylish': return stylish;
     case 'plain': return plain;
+    case 'json': return json;
     default:
       throw new Error(`unsupported format: ${format}`);
   }
