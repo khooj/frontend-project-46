e5083baac8cba1ff632fe632b826c10a1949eb45
Igor Gilmutdinov <bladoff@gmail.com>
2023-04-03 19:43:09 +0300

    Fixes tests.

--- __tests__/gendiff.test.js
+++ __tests__/gendiff.test.js
@@ -3,7 +3,6 @@ import { test, expect, beforeEach } from '@jest/globals';
 import { fileURLToPath } from 'url';
 import fs from 'fs';
 import genDiff from '../src/diff.js';
-import parse from '../src/parsers/parse.js';
 
 const __filename = fileURLToPath(import.meta.url);
 const __dirname = dirname(__filename);
@@ -11,28 +10,25 @@ const base = path.join(__dirname, '..', '__fixtures__');
 
 const expectedOutput = fs.readFileSync(path.join(base, 'result.txt'), 'utf8');
 const expectedPlainOutput = fs.readFileSync(path.join(base, 'result_plain.txt'), 'utf8');
-let obj1; let
-  obj2;
+let file1; let
+  file2;
 
 beforeEach(() => {
-  const file1 = path.join(base, 'file1.json');
-  const file2 = path.join(base, 'file2.json');
-
-  obj1 = parse(file1);
-  obj2 = parse(file2);
+  file1 = path.join(base, 'file1.json');
+  file2 = path.join(base, 'file2.json');
 });
 
 test('gendiff json', () => {
-  const result = genDiff(obj1, obj2, 'stylish');
+  const result = genDiff(file1, file2, 'stylish');
   expect(result).toBe(expectedOutput);
 });
 
 test('gendiff yaml', () => {
-  const result = genDiff(obj1, obj2, 'stylish');
+  const result = genDiff(file1, file2, 'stylish');
   expect(result).toBe(expectedOutput);
 });
 
 test('gendiff plain', () => {
-  const result = genDiff(obj1, obj2, 'plain');
+  const result = genDiff(file1, file2, 'plain');
   expect(result).toBe(expectedPlainOutput);
 });
